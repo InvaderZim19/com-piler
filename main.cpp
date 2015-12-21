655f26af61f1299b6be7ebf04e47eee7db1099ce
Karl Cassel <thee1gameboy@yahoo.com>
2015-12-21 11:50:43 -0800

    part2
    we are tasked with creating a scanner. this creates tokens out of the
    files being compiled.
    
    we retrofitted our main to have the requirements and to output the
    correct files. we now need a ".tok" file to be put out.
    
    there is also a dummy parser we are given, we will actually be creating
    the parser in the next assignment.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@
 // Print out each input line read in, then strtok it for
 // tokens.
 
+/* Karl Cassel (1372617) kcassel
+   Wesly Lim   (1366779) welim
+   Program 2
+   10/20/2015 */
+
 using namespace std;
 
 #include <errno.h>
@@ -19,20 +24,15 @@ using namespace std;
 
 #include "auxlib.h"
 #include "stringset.h"
+#include "astree.h"
+#include "lyutils.h"
+#include "parser.h"
 
 const string CPP = "/usr/bin/cpp";
 const size_t LINESIZE = 1024;
-
-char *tempOptarg = NULL;
-bool dCheck = false; 
-
-// Chomp the last character from a buffer if it is delim.
-void chomp (char* string, char delim) {
-   size_t len = strlen (string);
-   if (len == 0) return;
-   char* nlpos = string + len - 1;
-   if (*nlpos == delim) *nlpos = '\0';
-}
+extern int yy_flex_debug;
+extern FILE* yyin;
+FILE* tokFile;
 
 // used https://www.safaribooksonline.com/library/view/c-cookbook/0596007612/ch10s17.html for reference
 // parses the filename without the suffix and adds a new extension
@@ -43,48 +43,35 @@ void changeExt(string& s, const string& newExt) {
    }
 }
 
-
-// Run cpp against the lines of the file.
-void cpplines (FILE* pipe, char* filename) {
-   int linenr = 1;
-   char inputname[LINESIZE];
-   strcpy (inputname, filename);
-
-   // goes through line by line until there are no more to go through
-   for (;;) {
-      char buffer[LINESIZE];
-      char* fgets_rc = fgets (buffer, LINESIZE, pipe);
-      if (fgets_rc == NULL) break;
-      chomp (buffer, '\n');
-      char* savepos = NULL;
-      char* bufptr = buffer;
-
-      // checks each token in the line through fgets
-      for (int tokenct = 1;; ++tokenct) {
-         char* token = strtok_r (bufptr, " \t\n", &savepos);
-         bufptr = NULL;
-         if (token == NULL) break;
-         intern_stringset(token);
-      }
-      ++linenr;
+// function to check to see if the typed file is a .oc file
+int ocCheck(char* filename){
+   std::string temp(filename);
+   std::size_t pos = temp.find(".");
+   if(temp.substr(pos).compare(".oc")){
+      fprintf(stderr, "Error: file is not a .oc file \n");
+      return get_exitstatus();
+   } else {
+      return 1;
    }
 }
 
 int main (int argc, char** argv) {
 
    // goes through each option for debugging
+   char *tempOptarg = NULL;
+   bool dCheck = false; 
    int t;
+   yy_flex_debug = 0;
    while((t = getopt(argc, argv, "ly@:D:")) != -1)
    {
       switch(t) {
-
-      // does nothing yet for -ly
       case 'l':
+         yy_flex_debug = 1;
          break;
       case 'y':
+         yyparse();
+         yydebug = 1;
          break;
-
-      // sets debug flags
       case '@':
          set_debugflags(optarg);
          break;
@@ -92,13 +79,11 @@ int main (int argc, char** argv) {
          dCheck = true;
          tempOptarg = optarg;
          break;
-
-      // lets user know if they entered something invalid
       case '?':
-         fprintf(stderr, "Error: incorrect input");
+         fprintf(stderr, "Error: incorrect input \n");
          return get_exitstatus();
       default: 
-         fprintf(stderr, "Error: incorrect input default");
+         fprintf(stderr, "Error: incorrect input default \n");
          return get_exitstatus();
       }
    }
@@ -106,31 +91,51 @@ int main (int argc, char** argv) {
    // goes through the options until a file is read
    set_execname (argv[0]);
    for (int argi = optind; argi < argc; ++argi) {
+
+      // variables for the CPP
       char* filename = argv[argi];
       string command;
+      char* filen = basename(filename);
+
+      // creates the program.tok file
+      std::string tok(filen);
+      changeExt(tok, "tok");
+      tokFile = fopen(tok.c_str(), "w");
+
+      // creates the program.str file
+      std::string str(filen);
+      changeExt(str, "str");
+      FILE* strfile = fopen(str.c_str(), "w");
 
       // preprocesses the program using CPP
       if(dCheck == false) {
-    	command = CPP + " " + filename;
+          command = CPP + " " + filename;
       } else {
-        command = CPP + " -D " + tempOptarg + " " + filename;
+          command = CPP + " -D " + tempOptarg + " " + filename;
       }
-      FILE* pipe = popen (command.c_str(), "r");
-      if (pipe == NULL) {
-         syserrprintf (command.c_str());
-      }else {
+      yyin = popen (command.c_str(), "r");
 
-         // after file opens, interns all the strings, then dumps
-         // the hash table into a file "program".str
-         cpplines (pipe, filename);
-         int pclose_rc = pclose (pipe);
+      if (yyin == NULL) {
+         syserrprintf (command.c_str());
+      } else {
+         
+         // checks to see if the file is specifically a .oc file
+         ocCheck(filename);
+
+         // loops through file adding tokens to program.tok and creating astree. stops at end of file
+         int z; 
+         while((z = yylex())){
+            if(z == YYEOF){
+               break;
+            }
+         }
+         
+         // after file opens, interns all the strings, then dumps the hash table into a file "program".str
+         int pclose_rc = pclose (yyin);
          eprint_status (command.c_str(), pclose_rc);
-         char* filen = basename(filename);
-	      std::string str(filen);
-	      changeExt(str, "str");
-	      FILE* strfile = fopen(str.c_str(), "w");
-	      dump_stringset(strfile);
-	      fclose(strfile);
+         dump_stringset(strfile);
+         fclose(strfile);
+         fclose(tokFile);
       }
    }  
    return get_exitstatus();
